175287cea338ed23a13718a70c14a0be75fc17c5
Adilfarooque <Adilfarq11@gmail.com>
2024-04-30 16:57:41 +0530

    Setting Up the broadcast side

--- server/signalling.go
+++ server/signalling.go
@@ -2,9 +2,10 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 // AllRooms is the global hashmap for the server
@@ -12,6 +13,7 @@ var AllRooms RoomMap
 
 // CreateRoomRequest Create a Room and returns roomID
 func CreateRoomRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	roomID := AllRooms.CreateRoom()
 
 	type resp struct {
@@ -22,7 +24,65 @@ func CreateRoomRequest(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(resp{RoomID: roomID})
 }
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type broadcastMsg struct {
+	Message map[string]interface{}
+	RoomID  string
+	Client  *websocket.Conn
+}
+
+var broadcast = make(chan broadcastMsg)
+
+func broadcaster() {
+	for {
+		msg := <-broadcast
+
+		for _, client := range AllRooms.Map[msg.RoomID] {
+			if client.Conn != msg.Client {
+				err := client.Conn.WriteJSON(msg.Message)
+				if err != nil {
+					log.Fatal(err)
+					client.Conn.Close()
+				}
+			}
+		}
+	}
+}
+
 // JoinRoomRequest will join client in a particular room
 func JoinRoomRequest(w http.ResponseWriter, r *http.Request) {
-	
+	roomID, ok := r.URL.Query()["roomID"]
+
+	if !ok {
+		log.Println("roomID missing in URL Parameters")
+		return
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Fatal("Web socket upgrade Error", err)
+	}
+
+	AllRooms.InsertIntoRoom(roomID[0], false, ws)
+
+	go broadcaster()
+
+	for {
+		var msg broadcastMsg
+		err := ws.ReadJSON(&msg.Message)
+		if err != nil {
+			log.Fatal("Read Error", err)
+		}
+		msg.Client = ws
+		msg.RoomID = roomID[0]
+
+		broadcast <- msg
+	}
+
 }
