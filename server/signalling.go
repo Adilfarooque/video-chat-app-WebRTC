b7ccc3523b909bd1b0e9b8049a6cc7d757d8d01a
Adilfarooque <Adilfarq11@gmail.com>
2024-04-30 14:00:08 +0530

    Updated the Room-Request handler

--- server/signalling.go
+++ server/signalling.go
@@ -1,19 +1,28 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-//AllRooms is the global hashmap for the server
+// AllRooms is the global hashmap for the server
 var AllRooms RoomMap
 
 // CreateRoomRequest Create a Room and returns roomID
-func CreateRoomRequest(w http.ResponseWriter , r *http.Request){
-	fmt.Fprintf(w,"Hello Create")
+func CreateRoomRequest(w http.ResponseWriter, r *http.Request) {
+	roomID := AllRooms.CreateRoom()
+
+	type resp struct {
+		RoomID string `json:"room_id"`
+	}
+
+	log.Println(AllRooms.Map)
+	json.NewEncoder(w).Encode(resp{RoomID: roomID})
 }
 
 // JoinRoomRequest will join client in a particular room
-func JoinRoomRequest(w http.ResponseWriter , r *http.Request){
-	fmt.Fprintf(w,"Hello Room")
+func JoinRoomRequest(w http.ResponseWriter, r *http.Request) {
+	
 }
